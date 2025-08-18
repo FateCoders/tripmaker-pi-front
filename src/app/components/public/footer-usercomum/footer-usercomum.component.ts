4ef9d812ceed325e8b62cb59f1d882d111349679
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-08-18 17:55:46 -0300

    feat: add FooterUsercomum component with HTML, SCSS, and unit tests

---
+++ src/app/components/public/footer-usercomum/footer-usercomum.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-usercomum',
+  imports: [],
+  templateUrl: './footer-usercomum.component.html',
+  styleUrl: './footer-usercomum.component.scss'
+})
+export class FooterUsercomumComponent {
+
+}
