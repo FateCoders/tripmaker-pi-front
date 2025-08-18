f4ec59428841447d89a1ad5d8ae669d85eb232b1
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-08-18 16:31:25 -0300

    feat: enhance Not Found page with Lottie animation and styling
    - Added Lottie animation support in app configuration.
    - Updated Not Found component HTML to include Lottie animation and a user-friendly message.
    - Styled Not Found component for better visual appeal and user experience.
    - Implemented navigation back to the home page from the Not Found page.

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AnimationOptions } from 'ngx-lottie';
+
+import { LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [LottieComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
 
+  options: AnimationOptions = {
+    path: '/assets/lottie/404_animation.json',
+  };
+
+  constructor(private router: Router) {}
+
+  navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
 }
