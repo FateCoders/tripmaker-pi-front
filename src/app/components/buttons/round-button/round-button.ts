76a04b413b01b369bce9e719648abf34f488720c
luizfiuzaa <luizfiuza.b@gmail.com>
2025-08-20 16:57:47 -0300

    :rocket: melhorando o round-button

--- src/app/components/buttons/round-button/round-button.ts
+++ src/app/components/buttons/round-button/round-button.ts
@@ -1,17 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-round-button',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './round-button.html',
   styleUrl: './round-button.scss',
 })
-
 export class RoundButton {
   @Input() icon: string = '';
-  @Input() href: string = '';
   @Input() label: string = '';
   @Input() isDisabled: boolean = false;
   @Input() onClick: () => void = () => {};
