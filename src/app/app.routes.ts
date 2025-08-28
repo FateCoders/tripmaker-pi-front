f229b503be6749ed9a425440bfc8aa7d2d7b8b70
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-08-28 17:04:29 -0300

    :rocket: :feat Criando a Landing-Page e configurando a sua rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
     {
         path: '', 
         component: HomeComponent,
         title: 'Página Inicial' 
+    },
+    {
+        path: 'landing-page', 
+        component: LandingComponent,
+        title: 'Landing Page' 
     },
     {
         path: '**', 
---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RoundButton } from "../../components/buttons/round-button/round-button";
+
+@Component({
+  selector: 'app-landing',
+  imports: [RoundButton],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+
+}
