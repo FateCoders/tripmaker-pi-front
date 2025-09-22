362704d554acf98cc84eb17076695ae777c41445
luizfiuzaa <luizffreitas2006@gmail.com>
2025-09-22 16:20:54 -0300

    :rocket: abstraindo round-button

--- src/app/components/buttons/round-button/round-button.ts
+++ src/app/components/buttons/round-button/round-button.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { CommonModule, NgStyle } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-round-button',
+  standalone: true,
   imports: [MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './round-button.html',
   styleUrl: './round-button.scss',
@@ -13,5 +14,21 @@ export class RoundButton {
   @Input() icon: string = '';
   @Input() label: string = '';
   @Input() isDisabled: boolean = false;
-  @Input() onClick: () => void = () => {};
+
+  @Input() width: string = 'auto';
+  @Input() height: string = 'auto';
+
+
+  @Output() onClick = new EventEmitter<MouseEvent>();
+
+  onButtonClick(event: MouseEvent): void {
+    this.onClick.emit(event);
+  }
+
+  getButtonStyles() {
+    return {
+      width: this.width,
+      height: this.height,
+    };
+  }
 }
