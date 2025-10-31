f41275bd5f0a63cc095ca15aa7c8cd62433fcae4
luizfiuzaa <luizffreitas2006@gmail.com>
2025-10-30 21:55:18 -0300

    :rocket: começando task

--- src/app/pages/users/tourism-promoter/tourism-promoter-home/tourism-promoter-home.ts
+++ src/app/pages/users/tourism-promoter/tourism-promoter-home/tourism-promoter-home.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
-import { FooterUsercomumComponent } from "../../../../components/public/bottom-menu/bottom-menu.component";
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RoutesService } from '../../../../services/routes.service';
+import { ListView } from '../../../../components/list-view/list-view';
+import { TabsList } from '../../../../components/tabs-list/tabs-list';
+import { HeaderTitle } from '../../../../components/header-title/header-title';
+import { FooterUsercomumComponent } from '../../../../components/public/bottom-menu/bottom-menu.component';
+import { TabsListCard } from '../../../../models/tabs-list-card';
+import { TabsSection } from '../../../../models/tabs-section';
 
 @Component({
   selector: 'app-tourism-promoter-home',
-  imports: [FooterUsercomumComponent],
+  imports: [FooterUsercomumComponent, ListView, HeaderTitle, TabsList, MatListModule],
   templateUrl: './tourism-promoter-home.html',
-  styleUrl: './tourism-promoter-home.scss'
+  styleUrl: './tourism-promoter-home.scss',
 })
 export class TourismPromoterHome {
+  private routesService = inject(RoutesService);
 
+  activeTab: string = 'Eventos';
+  availableRoutes: any[] = [];
+  currentItems: TabsListCard[] = [];
+
+  tabs: TabsSection[] = [
+    {
+      label: 'Eventos',
+      content: [
+        {
+          id: '1',
+          img: 'assets/images/jpg/teatro.jpeg',
+          title: 'Festival de Teatro de Tatuí',
+          description: 'Espetáculos gratuitos e pagos com...',
+          category: 'A',
+        },
+        {
+          id: '2',
+          img: 'assets/images/png/conservatorio.png',
+          title: 'Concerto no Conservatório',
+          description: 'Apresentação da orquestra jovem do...',
+          category: 'B',
+        },
+      ],
+    },
+    {
+      label: 'Rotas',
+      content: [],
+    },
+  ];
+
+  ngOnInit(): void {
+    this.currentItems = this.tabs[0].content;
+    this.loadRoutes();
+  }
+
+  loadRoutes() {
+    this.availableRoutes = this.routesService.getVisibleRoutes();
+  }
+
+  onTabChanged(index: number): void {
+    this.activeTab = this.tabs[index].label;
+    this.currentItems = this.tabs[index].content;
+  }
+
+  subscribeToRoute(routeId: string) {
+    if (this.routesService.registerUserToRoute(routeId)) {
+      console.log('Inscrição realizada com sucesso na rota!');
+    } else {
+      console.log('Inscrição falhou: você já está inscrito nesta rota.');
+    }
+  }
 }
--- src/app/services/routes.service.ts
+++ src/app/services/routes.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, inject } from "@angular/core";
 import { AuthService } from "./auth.service";
+import { Injectable, inject } from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
