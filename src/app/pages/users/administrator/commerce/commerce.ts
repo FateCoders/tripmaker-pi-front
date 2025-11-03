46f09881f174a184a2e3e9facc252902ca61260c
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-11-03 14:26:47 -0300

    :sparkles: :feat Rota de comércios

--- src/app/pages/users/administrator/commerce/commerce.ts
+++ src/app/pages/users/administrator/commerce/commerce.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommerceService } from '../../../../services/commerce.service';
-import { Router } from 'express';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Commerce } from '../../../../interfaces/commerce';
 import { HeaderTitle } from '../../../../components/header-title/header-title';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { FooterUsercomumComponent } from "../../../../components/public/bottom-menu/bottom-menu.component";
+import { FooterUsercomumComponent } from '../../../../components/public/bottom-menu/bottom-menu.component';
 
 @Component({
   selector: 'app-administrator-commerce',
@@ -16,5 +16,29 @@ import { FooterUsercomumComponent } from "../../../../components/public/bottom-m
   styleUrl: './commerce.scss',
 })
 export class AdministratorCommerce {
-  
+  private commerceService = inject(CommerceService);
+  private router = inject(Router);
+
+  commerces$!: Observable<Commerce[]>;
+  isLoading = true;
+
+  ngOnInit(): void {
+    this.commerces$ = this.commerceService.getAllCommercesForUser();
+    this.commerces$.subscribe(() => (this.isLoading = false));
+  }
+
+  getRatingStars(rating: number): boolean[] {
+    return Array(5)
+      .fill(false)
+      .map((_, i) => i < rating);
+  }
+
+  onCommerceClick(commerce: Commerce): void {
+    this.commerceService.selectCommerce(commerce.id);
+    this.router.navigate(['/administrador/inicio']);
+  }
+
+  registerNewCommerce(): void {
+    this.router.navigate(['/administrador/comercios/cadastro']);
+  }
 }
