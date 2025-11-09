132979c67d49ccaf833262bfa86c6be09c84a321
kits93 <joaomarins93@gmail.com>
2025-11-09 14:05:59 -0300

    Criando componente e tela para avaliações (reviews)

---
+++ src/app/components/card-review/card-review.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-review',
+  imports: [],
+  templateUrl: './card-review.html',
+  styleUrl: './card-review.scss'
+})
+export class CardReview {
+
+}
