292c531ecec47a33d60d7ef08a902c87888db7b8
kits93 <joaomarins93@gmail.com>
2025-11-22 12:26:51 -0300

    Definindo url para api do backend nos environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   googleMapsApiKey: 'AIzaSyDx5IIQxxQ5nauqg1S-id_14ilVUp5sve4',
+  backendApiUrl: 'https://tripmaker-pi-back-production.up.railway.app/api',
 };
